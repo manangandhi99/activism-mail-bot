13a52cba8c673978a0e067c20f2346720c081386
sjhangiani12 <sjhangiani12@users.noreply.github.com>
2020-06-03 22:26:21 -0700

    added API functionality

--- send.py
+++ send.py
@@ -3,86 +3,52 @@ import messages, recipients, smtplib, ssl, time
 from getpass import getpass
 from email.message import EmailMessage
 
-def prompt_login():
-    print("\n")
-    name = ""
-    # Validate name is not blank
-    while True:
-        if not name:
-            name = input("Type your name and press enter: ")
+class Sender():
+
+    def get_recipients(self, state, county):
+        if state != None:        
+            if county != None:
+                recv = recipients.get_county(state, county)
+            else:
+                recv = recipients.get_state(state)
         else:
-            break
-
-    # Maybe add some validation for "@gmail" suffix
-    email = input("Type your email and press enter: ")
-    password = getpass("Type your password and press enter: ")
-    return name, email, password
+            recv = recipients.get_all()
+        return recv
 
 
+    def send_email(self, recv, src_name, src_email, password):
+        port = 465 # standard port for SMTP over SSL
+        smtp_server = "smtp.gmail.com"
 
-def prompt_email():
-    print("\nWhat would you like the subject (title) of your email to be?")
-    subject = input("Type here and press enter (if blank, a random one will be generated): ")
-    return subject
+        num_sent = 0
+        while True:
+            try:
+                # create a secure SSL context
+                context = ssl.create_default_context()
 
+                with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+                    server.login(src_email, password)
+                    while recv:
+                        recipient = recv.pop()
+                        dst_name = recipient[0]
+                        location = recipient[1]
+                        dst_email = recipient[2]
 
-def prompt_recipients():
-    print("\nWhich state officials do you want to send emails to?\n")
-    state_options = { v:k for v,k in enumerate(recipients.get_states()) }
-    for idx, opt in state_options.items():
-        print(idx, "->", opt)
-    
-    state_idx = input("\nType the number corresponding to the state here (if blank, all states will be used): ")
-    
-    if state_idx:
-        county_options = { v:k for v,k in enumerate(recipients.get_counties(state_options[int(state_idx)])) }
-        print("\nWhich county officials do you want to send emails to?\n")
-        for idx, opt in county_options.items():
-            print(idx, "->", opt)
-        county_idx = input("\nType the number corresponding to the county here (if blank, all counties will be used): ")
-    
-        if county_idx:
-            recv = recipients.get_county(state_options[int(state_idx)], county_options[int(county_idx)])
-        else:
-            recv = recipients.get_state(state_options[int(state_idx)])
-    
-    else:
-        recv = recipients.get_all()
-    return recv
+                        msg = EmailMessage()
 
-
-port = 465 # standard port for SMTP over SSL
-smtp_server = "smtp.gmail.com"
-
-recv = prompt_recipients()
-subject = prompt_email()
-src_name, src_email, password = prompt_login()
-
-while True:
-    try:
-        # create a secure SSL context
-        context = ssl.create_default_context()
-
-        with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-            server.login(src_email, password)
-            while recv:
-                recipient = recv.pop()
-                dst_name = recipient[0]
-                location = recipient[1]
-                dst_email = recipient[2]
-
-                msg = EmailMessage()
-
-                msg['Subject'] = subject if subject else messages.gen_subject()
-                msg['From'] = src_email
-                msg['To'] = dst_email
-                
-                body = messages.gen_body(src_name, dst_name, location)
-                msg.set_content(body)
-                print(msg.as_string())
-
-                server.send_message(msg)
-        break
-    except smtplib.SMTPException:
-        print("Unexpected error... trying again in 10 seconds.")
-        time.sleep(10)
+                        msg['Subject'] = subject if subject else messages.gen_subject()
+                        msg['From'] = src_email
+                        msg['To'] = dst_email
+                        
+                        body = messages.gen_body(src_name, dst_name, location)
+                        msg.set_content(body)
+                        print(msg.as_string())
+                        server.send_message(msg)
+                        print("Sent email")
+                        num_sent += 1
+                break
+            except smtplib.SMTPException:
+                print("Unexpected error... trying again in 10 seconds.")
+                time.sleep(10)
+        server.quit()
+        return num_sent
---
+++ server.py
@@ -0,0 +1,63 @@
+from flask import Flask, jsonify, request
+from waitress import serve
+
+from error import InvalidUsage
+from send import Sender
+
+app = Flask(__name__)
+
+
+def has_args(iterable, args):
+    """Verify that all args are in the iterable."""
+
+    try:
+        return all(x in iterable for x in args)
+
+    except TypeError:
+        return False
+
+
+@app.route('/', methods=['GET'])
+def ping():
+    return 'success'
+
+
+@app.errorhandler(InvalidUsage)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
+
+
+@app.route('/sendEmail', methods=['POST'])
+def send_email():
+
+    if not has_args(request.json, ['email']):
+        raise InvalidUsage('Missing email.')
+
+    if not has_args(request.json, ['password']):
+        raise InvalidUsage('Missing password.')
+    
+    if not has_args(request.json, ['name']):
+        raise InvalidUsage('Missing name.')
+
+    if not has_args(request.json, ['state']):
+        raise InvalidUsage('Missing state.')
+
+    if not has_args(request.json, ['county']):
+        raise InvalidUsage('Missing county.')
+
+    sender = Sender()
+    
+    state = request.json['state']
+    county = request.json['county']
+
+    recv = sender.get_recipients(state, county)
+    num_sent = sender.send_email(recv, request.json['name'], request.json['email'], request.json['password'])
+    
+    
+    return jsonify({'number of emails sent': num_sent})
+
+
+
+serve(app, host='0.0.0.0', port=3000)
